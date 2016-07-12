b87cae1421d1613b8f0ec237b2013b02508504c5
Brendan L <drac@dracsoft.com>
2016-07-12 14:33:12 -0400

    added ability to smiley-ize any image url by adding #assembleicon or #assembleiconsmall to the end

--- static/index.js
+++ static/index.js
@@ -857,6 +857,13 @@ function appendChatMessage(uid, room, roomname, nick, m, id, avatar, time, mode)
                         {
                             if (noImages)
                                 return "<a href='"+href+"' target='_blank'>"+href+"</a>";
+				
+			    if (match.getUrl().indexOf( '#assembleiconsmall' ) !== -1) {
+				return "<img src='"+href+"' class='smiley'></img>";
+			    }
+			    if (match.getUrl().indexOf( '#assembleicon' ) !== -1) {
+                                return "<img src='"+href+"' class='smiley large'></img>";
+                            }
 
                             return "<a href='"+href+"' target='_blank'>"+href+"</a><br><img src='"+href+"' class='autolink"+small+"'></img>";
                         }
